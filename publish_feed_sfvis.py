2858d2efb92942b747464078dbf57c7d57ff1eeb
Caique <157047818+cctrivelato@users.noreply.github.com>
2024-11-21 08:58:37 -0600

    Update publish_feed_sfvis.py

--- publish_feed_sfvis.py
+++ publish_feed_sfvis.py
@@ -16,7 +16,7 @@ frame2 = None
 
 sfvis = "01" # This value is unique for each SFVIS board
 camera_id1 = 0  # This value will never change
-camera_id2 = 4  # This value will never change, but check if it's correct
+camera_id2 = 2  # This value will never change, but check if it's correct
 
 db_config = {
             'user': 'root',
